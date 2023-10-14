a25a112a5564429cc2c641bd5e9bbedd0303f400
Kepha Kibet <kibet.kepha@live.com>
2023-10-14 17:40:09 +0300

    add clarity

--- issue-tracker/src/app/app.module.ts
+++ issue-tracker/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from
+ '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
