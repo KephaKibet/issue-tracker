740a2940ee0fe8e2c99b3b70108eded6fdf75f32
Kepha Kibet <kibet.kepha@live.com>
2023-10-15 16:26:48 +0300

    report new issue feature

--- issue-tracker/src/app/app.module.ts
+++ issue-tracker/src/app/app.module.ts
@@ -3,21 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from
  '@angular/platform-browser/animations';
-
+import { ReactiveFormsModule } from '@angular/forms';
+ 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueReportComponent } from './issue-report/issue-report.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IssueListComponent
+    IssueListComponent,
+    IssueReportComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ClarityModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
