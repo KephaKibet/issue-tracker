be22253e6070e58c7f73210711df2dd90ab5dfc2
Kepha Kibet <kibet.kepha@live.com>
2023-10-16 18:07:59 +0300

    Validating input details of new issue

--- issue-tracker/src/app/issue-report/issue-report.component.ts
+++ issue-tracker/src/app/issue-report/issue-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Issue } from '../issue';
 
 import { IssuesService } from '../issues.service';
@@ -20,17 +20,27 @@ interface IssueForm {
 
 export class IssueReportComponent {
   issueForm = new FormGroup<IssueForm>({
-    title: new FormControl('', { nonNullable: true }),
-    description: new FormControl('', { nonNullable: true }),
-    priority: new FormControl('', { nonNullable: true }),
-    type: new FormControl('', { nonNullable: true })
+    title: new FormControl('', { nonNullable: true, validators:
+      Validators.required }),
+    description: new FormControl('', { nonNullable: true, validators:
+      Validators.required }),
+    priority: new FormControl('', { nonNullable: true, validators:
+      Validators.required }),
+    type: new FormControl('', { nonNullable: true, validators:
+      Validators.required })
    });
  
+  
+  //  notify IssueListComponent of new issue
   @Output() formClose = new EventEmitter();
 
   constructor(private issueService: IssuesService) { }
 
-   addIssue() {
+  addIssue() {
+    if (this.issueForm && this.issueForm.invalid) {
+      this.issueForm.markAllAsTouched();
+      return;
+    }
     this.issueService.createIssue(this.issueForm.getRawValue() as
       Issue);
       this.formClose.emit();
