8e140aec12ef19e008c7bead668b7e76ed0141ce
Kepha Kibet <kibet.kepha@live.com>
2023-10-20 16:05:48 +0300

    feat:suggestions for new issues

--- issue-tracker/src/app/issue-report/issue-report.component.ts
+++ issue-tracker/src/app/issue-report/issue-report.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Issue } from '../issue';
 
@@ -18,7 +18,10 @@ interface IssueForm {
   styleUrls: ['./issue-report.component.css']
 })
 
-export class IssueReportComponent {
+export class IssueReportComponent implements OnInit {
+  
+  suggestions: Issue[] = [];
+  
   issueForm = new FormGroup<IssueForm>({
     title: new FormControl('', { nonNullable: true, validators:
       Validators.required }),
@@ -35,6 +38,12 @@ export class IssueReportComponent {
   @Output() formClose = new EventEmitter();
 
   constructor(private issueService: IssuesService) { }
+
+  ngOnInit(): void {
+ this.issueForm.controls.title.valueChanges.subscribe(title => {
+ this.suggestions = this.issueService.getSuggestions(title);
+ });
+}
 
   addIssue() {
     if (this.issueForm && this.issueForm.invalid) {
--- issue-tracker/src/app/issues.service.ts
+++ issue-tracker/src/app/issues.service.ts
@@ -13,6 +13,7 @@ export class IssuesService {
   getPendingIssues(): Issue[] {
     return this.issues.filter(issue => !issue.completed);
   }
+
   createIssue(issue: Issue) {
     issue.issueNo = this.issues.length + 1;
     this.issues.push(issue);
@@ -25,5 +26,13 @@ export class IssuesService {
     };
     const index = this.issues.findIndex(i => i === issue);
     this.issues[index] = selectedIssue;
-   }
+  }
+  
+  getSuggestions(title: string): Issue[] {
+    if (title.length > 3) {
+      return this.issues.filter(issue =>
+        issue.title.indexOf(title) !== -1);
+    }
+    return [];
+  }
 }
